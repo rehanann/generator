87fb6d1eb955f8fcdc81a3c42180347eac9ff2e8
Rehan Anwar <rehanann@gmail.com>
2017-08-13 19:42:59 +0000

    first commit

---
+++ lcd_origin/metadata.rb
@@ -0,0 +1,5 @@
+name             'lcd_origin'
+description      'Custom code generator cookbook for use with ChefDK'
+long_description 'Custom code generator cookbook for use with ChefDK'
+version          '0.1.0'
+
